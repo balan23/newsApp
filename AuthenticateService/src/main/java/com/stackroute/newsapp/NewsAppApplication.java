ffe1b9f68f4ae7625f9d041f8f31c44d08423871
Bhakti Rathod <bhakti.rathod@niit.com>
2018-12-06 15:05:42 +0530

    new boilerplate

---
+++ AuthenticateService/src/main/java/com/stackroute/newsapp/NewsAppApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.newsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAppApplication.class, args);
+	}
+}
---
+++ Server/src/test/java/com/stackroute/newsapp/NewsAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stackroute.newsapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewsAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
