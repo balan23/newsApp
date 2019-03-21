7e51a33926e20a2be824df7c7ac67334bc96c6f1
Manobalan A <manobalan.a@cognizant.com>
2019-03-21 16:52:45 +0530

    Removed unused imports

--- Server/src/main/java/com/stackroute/newsapp/controller/NewsController.java
+++ Server/src/main/java/com/stackroute/newsapp/controller/NewsController.java
@@ -2,12 +2,9 @@ package com.stackroute.newsapp.controller;
 
 import java.util.List;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
